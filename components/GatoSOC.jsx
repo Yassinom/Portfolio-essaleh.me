e538028bd0ebcb88d7d3470f3e79c2c7562a3d5d
Yassinom <essalehyassine@gmail.com>
2024-11-11 16:00:14 +0100

    modified:   components/GatoSOC.jsx 	modified:   package-lock.json 	modified:   package.json 	new file:   public/projects/GatoSOC.pdf

--- components/GatoSOC.jsx
+++ components/GatoSOC.jsx
@@ -3,8 +3,14 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { IoIosArrowDropdown } from "react-icons/io";
 
-import CardElement from "@/components/CardElement";
 import ProjectElement from "@/components/ProjectElement";
+import { VscDebugBreakpointData } from "react-icons/vsc";
+import Flickity from 'react-flickity-component'
+
+
+
+
+
 
 const GatoSOC = () => {
   const [rotated, setRotated] = useState(false);
@@ -14,6 +20,10 @@ const GatoSOC = () => {
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
+
+  const flickityOptions = {
+    initialIndex: 0,
+}
 
   return (
     <div className="md:max-w-[60%] max-w-[90%] mx-auto">
@@ -59,7 +69,7 @@ const GatoSOC = () => {
                         </div>
                     </div>
                 </div>
-              
+            
                 
       {/* Modal and Background Blur */}
       {isModalOpen && (
@@ -98,7 +108,7 @@ const GatoSOC = () => {
                   />
 
                   <div className="flex md:flex-row flex-col mt-14">
-                      <div className="md:w-2/3 w-[100%] px-3 rounded-[20px] hover:bg-[#919292] hover:bg-opacity-30 transition-all duration-200 mr-2">
+                      <div className="md:w-2/3 w-[100%] px-3 rounded-[20px] hover:bg-[#919292] hover:bg-opacity-30 transition-all duration-200 mr-2 pb-1">
                           <div className="text-[30px] font-bold ">Overview</div>
                           <div className="md:text-[21px] text-[15px] text-[#9f9fa0]">
                             This Next-Gen Security Operations Center aims to strengthen an organization&apos;s cybersecurity framework by offering real-time monitoring and incident response functionalities. This all-encompassing platform combines multiple open-source tools to deliver effective security monitoring and threat management solutions.
@@ -118,13 +128,139 @@ const GatoSOC = () => {
                             <ProjectElement skillName="VirusTotal "/>
                             <ProjectElement skillName="Yara"/>
                             <ProjectElement skillName="Sysmon"/>
+                            <ProjectElement skillName="PacketBeat"/>
+                            <ProjectElement skillName="ELK Stack"/>
                           </div>
                       </div>
                   </div>
 
+                  <div className="flex md:flex-row flex-col mt-10">
+                    <div className="md:w-2/3 w-[100%] px-3 rounded-[20px] hover:bg-[#919292] hover:bg-opacity-30 transition-all duration-200 mr-2 pb-1">
+                      <div className="md:text-[30px] text-[27px] font-bold mb-2">Accomplished Objectives</div>
+                      <div className="md:text-[19px] text-[15px] text-[#9f9fa0] space-y-2">
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Real-time log collection, analysis, and threat detection using Wazuh, Graylog, and ELK Stack
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Automated incident management and response using TheHive, Cortex, and VirusTotal
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Centralized security data visualization through Grafana and Kibana
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Scalable log ingestion and processing for high-volume security data analysis
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          End-to-end threat intelligence and alerting with TheHive's SOAR capabilities
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Enriched threat intelligence and alerting via TheHive's SOAR capabilities and VirusTotal
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="md:w-1/3 w-[100%] px-3 md:pt-0 pt-9 rounded-[20px]">
+                          <div className="text-[30px] font-bold mb-2">Project Details</div>
+                          
+                          <div className="flex justify-between md:text-[21px] text-[16px] text-white border-b border-[#414a58] pb-2">
+                            Team Members
+                            <div className="flex flex-col md:text-[21px] text-[15px] text-[#9f9fa0]">
+                              <div className="hover:text-white transition-all duration-200 cursor-pointer underline" onClick={() => window.open('https://www.linkedin.com/in/yassine-essaleh-a25749261/', '_blank')}>ESSALEH Yassine</div>
+                              <div className="hover:text-white transition-all duration-200 cursor-pointer underline" onClick={() => window.open('https://www.linkedin.com/in/mehdi-stoti/', '_blank')}>STOTI Mehdi</div>  
+                            </div> 
+                          </div>
+
+                          <div className="flex justify-between md:text-[21px] text-[16px] text-white border-b border-[#414a58] pb-2 mt-4">
+                            Date
+                            <div className="flex flex-col md:text-[21px] text-[15px] text-[#9f9fa0]">
+                              <div className="hover:text-white transition-all duration-200" >April 2024 - June 2024</div>
+                              
+                            </div> 
+                          </div>
+
+                          <div className="flex justify-between md:text-[21px] text-[16px] text-white border-b border-[#414a58] pb-2 mt-4">
+                            Context
+                            <div className="flex flex-col md:text-[21px] text-[15px] text-[#9f9fa0]">
+                              <div className="hover:text-white transition-all duration-200" >School Project</div>
+                              
+                            </div> 
+                          </div>
+
+                          <div className="flex justify-between md:text-[21px] text-[16px] text-white  mt-4">
+                            Report
+                            <div className="flex flex-col md:text-[21px] text-[15px] text-[#9f9fa0]">
+                              <div className="hover:text-white transition-all duration-200 cursor-pointer underline" onClick={() => window.open('/projects/GatoSOC.pdf', '_blank')}>GatoSOC</div>   
+                            </div> 
+                          </div>
+
+
+
+                      </div>
+                  </div>
+
+                  <div className="flex md:flex-row flex-col mt-10">
+                    <div className="md:w-1/2 w-[100%] px-3 rounded-[20px] hover:bg-[#919292] hover:bg-opacity-30 transition-all duration-200 mr-2 pb-1">
+                      <div className="md:text-[30px] text-[27px] font-bold mb-2">Addressed Challenges</div>
+                      <div className="md:text-[19px] text-[15px] text-[#9f9fa0] space-y-2">
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Regulatory compliance requirements fulfilled by Wazuh for PCI DSS, HIPAA, and GDPR.
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Incident response delays reduced with SOAR for automated responses.
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[40px] mr-2"/> 
+                          Inefficient log visualization improved by integrating Graylog with Elasticsearch/OpenSearch for centralized management.
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Inconsistent log formats standardized with Graylog Extended Log Format (GELF).
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[40px] mr-2"/> 
+                          Use of Graylog's plugins and content packs from its community marketplace, with over 8,000 members, addressed community support and extensibility needs.
+                        </div>
+                        <div className="flex items-center">
+                          <VscDebugBreakpointData className="text-[25px] mr-2"/> 
+                          Achieved a satisfaction level of 8/10, reflecting how well our goals were accomplished.
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="md:w-1/2 w-[100%] px-3 md:pt-0 pt-9 rounded-[20px]">
+                          <div className="text-[30px] font-bold mb-2">Gallery</div>
+                          
+                          <Flickity
+                            className={'carousel'} // default ''
+                            elementType={'div'} // default 'div'
+                            options={flickityOptions} // takes flickity options {}
+                            disableImagesLoaded={false} // default false
+                            reloadOnUpdate // default false
+                            static // default false
+                          >
+                            <img src="/images/naruto.jpeg"/>
+                            <img src="/images/GatoSOC.png"/>
+                            
+                          </Flickity>
+
+                          
+                      </div>
+                  </div>
+
+
+
                  
               </motion.div>
           </div>
+
       )}
     </div>
   );
